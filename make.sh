7ebc0733e9a85a223a46af03b0328e29fc2ad10f
CatPieLeaf <149953009+CatPieLeaf@users.noreply.github.com>
2025-04-30 00:15:54 -0300

    Fix multi plataform builds

--- make.sh
+++ make.sh
@@ -3,20 +3,24 @@ if [[ "$1" = "t" ]]; then
     go build -race -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
 elif [[ -n "$1" ]]; then
     CGO_ENABLED=0 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
-    GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
-    GOOS=linux GOARCH=arm64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
-    GOOS=windows GOARCH=arm64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
+    GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)" -o ytarchive_windows_amd64.exe
+    GOOS=windows GOARCH=arm64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)" -o ytarchive_windows_arm64.exe
+    GOOS=linux GOARCH=amd64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)" -o ytarchive_linux_amd64
+    GOOS=linux GOARCH=arm64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)" -o ytarchive_linux_arm64
+    GOOS=android GOARCH=arm64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)" -o ytarchive_android_arm64
 else
     CGO_ENABLED=0 go build
-    GOOS=windows GOARCH=amd64 go build
-    GOOS=linux GOARCH=arm64 go build
-    GOOS=windows GOARCH=arm64 go build
+    GOOS=windows GOARCH=amd64 go build -o ytarchive_windows_amd64
+    GOOS=windows GOARCH=arm64 go build -o ytarchive_windows_arm64
+    GOOS=linux GOARCH=amd64 go build -o ytarchive_linux_amd64
+    GOOS=linux GOARCH=arm64 go build -o ytarchive_linux_arm64
+    GOOS=android GOARCH=arm64 go build -o ytarchive_android_arm64
 fi
 
-zip ytarchive_linux_amd64.zip ytarchive
-zip ytarchive_windows_amd64.zip ytarchive.exe
-zip ytarchive_linux_arm64.zip ytarchive
-zip ytarchive_windows_arm64.zip ytarchive.exe
+zip ytarchive_windows_amd64.zip ytarchive_windows_amd64.exe
+zip ytarchive_windows_arm64.zip ytarchive_windows_arm64.exe
+zip ytarchive_linux_amd64.zip ytarchive_linux_amd64
+zip ytarchive_linux_arm64.zip ytarchive_linux_arm64
+zip ytarchive_android_arm64.zip ytarchive_android_arm64
 
-sha256sum ytarchive_linux_amd64.zip ytarchive_windows_amd64.zip \
-  ytarchive_linux_arm64.zip ytarchive_windows_arm64.zip > SHA2-256SUMS
+sha256sum ytarchive_windows_amd64.zip ytarchive_windows_arm64.zip ytarchive_linux_amd64.zip  ytarchive_linux_arm64.zip ytarchive_android_arm64.zip > SHA2-256SUMS
